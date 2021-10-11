fbf52b2f0fdccbc7a48bf73a84a757979d00cbef
Max <maks03022003@g,aim.com>
2021-10-11 11:12:59 +0200

    drugi commit

--- src/pl/vistula/helloworldBILOIVANOV/HelloWorldBILOIVANOV.java
+++ src/pl/vistula/helloworldBILOIVANOV/HelloWorldBILOIVANOV.java
@@ -3,6 +3,6 @@ package pl.vistula.helloworldBILOIVANOV;
 public class HelloWorldBILOIVANOV {
     public static void main(String[] args) {
         System.out.println("Hello Vistula Biloivanov");
-        System.out.println("Pierwszy commit");
+        System.out.println("Drugi commit");
     }
 }
