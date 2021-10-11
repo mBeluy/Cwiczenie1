8ec458b6cafa2fa65763743aaf715e78e862156f
Max <maks03022003@g,aim.com>
2021-10-11 11:10:54 +0200

    HelloVistulaBILOIVANOV

---
+++ src/pl/vistula/helloworldBILOIVANOV/HelloWorldBILOIVANOV.java
@@ -0,0 +1,8 @@
+package pl.vistula.helloworldBILOIVANOV;
+
+public class HelloWorldBILOIVANOV {
+    public static void main(String[] args) {
+        System.out.println("Hello Vistula Biloivanov");
+        System.out.println("Pierwszy commit");
+    }
+}
